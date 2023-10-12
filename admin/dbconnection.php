10d373e165f7709695a59a989967a88d199a1730
Boitumelo <126359326+BoitumeloSpider@users.noreply.github.com>
2023-10-12 08:42:53 +0200

    Update dbconnection.php

--- admin/dbconnection.php
+++ admin/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "yvygdgzqpg", "", "EDzVy9pTbT");
+$con=mysqli_connect("localhost", "yvygdgzqpg", "EDzVy9pTbT", "yvygdgzqpg");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
